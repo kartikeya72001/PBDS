07ff538fd36bcf010e96417d26c0989669afed41
kartikeya72001 <kartikeya72001@gmail.com>
2020-07-27 20:09:34 +0530

    CodeForcesQues

--- PairOfTopics.cpp
+++ PairOfTopics.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-typedef tree<int,  null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> PBDS;
+typedef tree<pair<int,int>,  null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> PBDS;
 //Works as an Ordered set
 int main(int argc, char const *argv[]) {
 
@@ -16,19 +16,30 @@ int main(int argc, char const *argv[]) {
         freopen("output.txt","w",stdout);
     #endif
 
-    PBDS st;
     int n;
     cin>>n;
-    int arr[n];
+    int a[n],b[n],c[n];
 
     for(int i=0;i<n;i++)
-        cin>>arr[i];
-    int inversion_count = 0;
+        cin>>a[i];
+    for(int i=0;i<n;i++)
+        cin>>b[i];
+    for(int i=0;i<n;i++)
+        c[i] = a[i] - b[i];
+
+    PBDS st;
+    int ans = 0;
     for(int i=0;i<n;i++)
     {
-        inversion_count += st.size() - st.order_of_key(arr[i]);
-        st.insert(arr[i]);
+        ans += st.size() - st.order_of_key({-c[i],1e8});
+        st.insert({c[i],i});
     }
-    cout<<inversion_count<<endl;
+    cout<<ans<<endl;
     return 0;
 }
+/*
+5
+4 8 2 6 2
+4 5 4 1 3
+
+*/
